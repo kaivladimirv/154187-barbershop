8ba66eeb66e6f72911b29993818f515d02dfa32e
Каймонов Владимир <kaivladimirv@gmail.com>
2016-05-07 18:07:05 +0600

    Завершаем вёрстку «Барбершопа»

---
+++ js/script.js
@@ -0,0 +1,57 @@
+var map = document.querySelector('.js-map');
+var btnOpenMap = document.querySelector('.js-btn-open-map');
+var btnCloseMap = document.querySelector('.js-btn-close-map');
+var btnCloseDialog = document.querySelectorAll('.js-modal-dialog-close');
+
+var dialogLogin = document.querySelector('.js-modal-dialog-login');
+var btnOpenDialogLogin = document.querySelector('.js-btn-open-login-form');
+var btnCloseDialogForm = document.querySelector('.js-btn-close-login-form');
+var formLogin = dialogLogin.querySelector('form');
+var login = formLogin.querySelector('input[name="login"]');
+var password = formLogin.querySelector('input[name="password"]');
+
+
+btnOpenMap.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  if (!map.classList.contains('is-show')) {
+    map.classList.add('is-show');
+  }
+
+});
+
+btnCloseMap.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  map.classList.remove('is-show');
+
+});
+
+btnOpenDialogLogin.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  if (!dialogLogin.classList.contains('is-show')) {
+    dialogLogin.classList.add('is-show');
+  }
+
+});
+
+btnCloseDialogForm.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  dialogLogin.classList.remove('is-show');
+
+});
+
+formLogin.addEventListener('submit', function(event) {
+  if (!login.value || !password.value) {
+    event.preventDefault();
+    console.log("Нужно ввести логин и пароль");
+  }
+});
+
+window.addEventListener('keydown', function(event) {
+  if (event.keyCode === 27) {
+    dialogLogin.classList.remove('is-show');
+  }
+});
